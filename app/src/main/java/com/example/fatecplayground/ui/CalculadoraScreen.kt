9e12f28bd70969222d3595e2c091a790d596ee94
H. Nunes <670202+h4mn@users.noreply.github.com>
2023-03-14 09:38:20 -0600

    add: adiciona componentes na calculadora

--- app/src/main/java/com/example/fatecplayground/ui/CalculadoraScreen.kt
+++ app/src/main/java/com/example/fatecplayground/ui/CalculadoraScreen.kt
@@ -1,19 +1,24 @@
 package com.example.fatecplayground.ui
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
+import androidx.compose.material.Card
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.fatecplayground.Greeting
-import com.example.fatecplayground.currentActivity
-import com.example.fatecplayground.ui.ui.theme.FatecPlaygroundTheme
+import com.example.fatecplayground.MainScreen
 
 @Composable
 fun CalculadoraScreen(
@@ -21,8 +26,53 @@ fun CalculadoraScreen(
     onClickSeeAllBills: () -> Unit = {},
     onAccountClick: (String) -> Unit = {},
 ) {
+    val valor_1: String by remember { mutableStateOf("1") }
+    val valor_2: String by remember { mutableStateOf("2") }
+    val valor_resultado: String by remember { mutableStateOf("resultado") }
     Column {
-        Greeting("Calculadora")
-        Text(text = "1 + 1 = 2")
+        Text(text = "Valor 1")
+        TextField(
+            value = valor_1,
+            onValueChange = {},
+            modifier = Modifier.fillMaxWidth()
+        )
+        Text(text = "Valor 2")
+        TextField(
+            value = valor_2,
+            onValueChange = {},
+            modifier = Modifier.fillMaxWidth()
+        )
+        Text(text = "Resultado")
+        TextField(
+            value = valor_resultado,
+            onValueChange = {},
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+
+            onClick = { /*TODO*/ }
+        ) {
+
+        }
     }
+}
+
+@Composable
+fun Visor (t: String) {
+    Card {
+        Text(
+            text = t,
+            style = TextStyle(
+                fontSize = 30.sp,
+                fontFamily = FontFamily.Monospace,
+                textAlign = TextAlign.Right
+            ),
+        )
+    }
+}
+
+@Preview
+@Composable
+fun CalculadoraPreview () {
+    MainScreen()
 }
