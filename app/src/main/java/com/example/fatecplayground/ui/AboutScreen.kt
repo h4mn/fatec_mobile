a942da90a81cea85ca0d3a38cb6897a4cd21df81
H. Nunes <670202+h4mn@users.noreply.github.com>
2023-05-05 09:24:14 -0300

    add: adiciona melhoria na AboutScreen

--- app/src/main/java/com/example/fatecplayground/ui/AboutScreen.kt
+++ app/src/main/java/com/example/fatecplayground/ui/AboutScreen.kt
@@ -1,15 +1,20 @@
 package com.example.fatecplayground.ui
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 
@@ -22,21 +27,41 @@ fun AboutScreen(
     modifier = Modifier
       .fillMaxHeight()
   ) {
-    Row(
-      modifier = Modifier
-        .fillMaxWidth()
-        .align(CenterHorizontally)
-    )
-    {
-      Text(
-        text = "Hadston Nunes",
-        modifier = Modifier
-          .fillMaxHeight()
-          .align(CenterVertically)
-      )
-    }
+    Linha(title = "Fatec",
+      description = "Faculdade de Tecnologia de Americana")
+    //Spacer(modifier = Modifier.height(8.dp))
+    Linha(title = "Autor",
+      description = "Hadston Nunes")
+    //Spacer(modifier = Modifier.height(8.dp))
+    Linha(title = "Fatec Playground",
+      description = "https://github.com/h4mn/fatec_mobile")
   }
 
+}
+
+@Composable
+fun Linha(
+  title: String,
+  description: String
+) {
+  Card(
+    modifier = Modifier
+      .padding(16.dp)
+      .fillMaxWidth(),
+    elevation = 8.dp
+  ) {
+    Column(
+      modifier = Modifier
+        .padding(16.dp)
+        .fillMaxWidth()
+    ) {
+      Text(text = title, fontWeight = FontWeight.Bold)
+      Spacer(modifier = Modifier.height(8.dp))
+      Row(verticalAlignment = CenterVertically) {
+        Text(text = description, style = MaterialTheme.typography.body1)
+      }
+    }
+  }
 }
 
 @Composable
